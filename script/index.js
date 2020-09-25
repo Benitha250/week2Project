c04b7ff2b655c7847f3a55b62ad8247ea24cddcb
Benitha250 <benithauwase05@gmail.com>
2020-09-25 12:51:49 -0400

    put Akan names and their days in arrays

--- script/index.js
+++ script/index.js
@@ -41,3 +41,22 @@ function getAkanName () {
       return true;
     }
   }
+  let monthValid = monthValidator();
+  let dayValid = dayValidator();
+
+  //Day of birth (Sunday = 1, Monday = 2 ...)
+  let dayOfWeekNumber = Math.floor((((Number(yearOfBirth.slice(0,2))/4)-2*Number(yearOfBirth.slice(0,2))-1)+
+          ((5*Number(yearOfBirth.slice(2,4))/4))+((26*(monthOfBirth+1)/10))+dayOfBirth)%7);
+
+//Arrays of Akan names for males & females and days of the week
+  let daysOfWeek = [
+    "Sunday", "Monday", "Tuesday", "Wednesday", "Thursday", "Friday", "Saturday"
+  ];
+
+  let maleAkanNames = [
+    "Kwasi", "Kwadwo", "Kwabena", "Kwaku", "Yaw", "Kofi", "Kwame"
+  ];
+
+  let femaleAkanNames = [
+    "Akosua", "Adwoa", "Abenaa", "Akua", "Yaa", "Afua", "Ama"
+  ];
